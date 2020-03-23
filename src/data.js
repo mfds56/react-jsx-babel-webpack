a9b780482cca3dbae9aef02c7dac42c4eb644b94
mfds56 <60048224+mfds56@users.noreply.github.com>
2020-03-23 11:49:26 -0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id: 'abc123',
+        nome: 'Pet da Fotudona',
+        descricao: 'Tosa e banho',
+        preco: '150',
+        imagem: 'https://girabetim.com.br/wp-content/uploads/2019/02/gato-cachorro2.png'
+    },
+    {
+        id: 'bbc123',
+        nome: 'Pet da Fox',
+        descricao: 'Corte das Unhas',
+        preco: '50',
+        imagem: 'https://www.petsa.com.br/source/files/c/11104/800-472-0-0.jpg'
+    },
+    {
+        id: 'cbc123',
+        nome: 'Pet da Fotuda',
+        descricao: 'Limpeza dos Dentes',
+        preco: '100',
+        imagem: 'https://vetloscolorados.com/wp-content/uploads/2016/11/pets_big.png'
+    }
+]
